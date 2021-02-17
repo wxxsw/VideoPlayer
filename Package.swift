3c5b9531ee20e34d3a86142ca2c076a5b99afdb2
Gesen <i@gesen.me>
2021-02-17 17:42:33 +0800

    fix: server error 416, download wrong range first index, close #23

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/wxxsw/GSPlayer.git", from: "0.2.24"),
+        .package(url: "https://github.com/wxxsw/GSPlayer.git", from: "0.2.25"),
     ],
     targets: [
         .target(
